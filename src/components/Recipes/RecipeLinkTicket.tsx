9904a090b48b9d694335b094a0239ef5731248ba
AmirAdarGit <amiradar12345@gmail.com>
2023-12-31 17:39:34 +0200

    add recipeLink &&

--- src/components/Recipes/RecipeLinkTicket.tsx
+++ src/components/Recipes/RecipeLinkTicket.tsx
@@ -25,7 +25,7 @@ export const RecipeLinkTicket: React.FC<Props> = ({recipeLink, index, colorIndex
       {recipeLink && <RecipeInfoRightSideWrapper colorIndex={colorIndex}>
           <RecipeInfoWrapper>
             <RecipeLinkNameStyled>
-              {recipeLink && recipeLink.recipeLinkName.toUpperCase() }
+              {recipeLink.recipeLinkName }
             </RecipeLinkNameStyled>
             <div>
               {recipeLink.recipeLinkDescription }
